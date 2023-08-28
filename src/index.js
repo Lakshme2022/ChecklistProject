d718fc06a1176e8626017da5825fd6205e935fcd
Katerina Sitnikov <lakshme@me.com>
2023-08-28 12:12:26 +0100

    Код и стили для оболочки страницы,  бегунков и кнопок

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+const content = document.getElementById('content');
+
+function range() {
+    return  '<div class="input indent">' +
+        '<input type="range" min="1" max="5" class="slider" value="1">' +
+        '<div class="numbers"><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div>' +
+        '</div>' +
+        '</div>';
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    content.innerHTML = range();
+});
