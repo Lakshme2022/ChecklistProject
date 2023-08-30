a449816f6b2fbf6deb421e0c7b60ba71709391c3
OlgaLightning <ol.laitinen@gmail.com>
2023-08-30 09:37:06 +0100

    added beginner page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
 const content = document.getElementById('content');
 
-function range() {
-    return  '<div class="input indent">' +
+export function range() { //Добавила export, чтобы импортировать функцию в страницы с вопросами
+    return '<div class="input indent">' +
         '<input type="range" min="1" max="5" class="slider" value="1">' +
         '<div class="numbers"><div>1</div><div>2</div><div>3</div><div>4</div><div>5</div>' +
         '</div>' +
---
+++ src/pages js/handleQuestionsFunction.js
@@ -0,0 +1,17 @@
+
+//Функция для вывода всех вопросов и бегунков на страницу
+export function handleQuestions() {
+
+    for (const question of questions) {
+        const questionDiv = document.createElement('div');
+        questionDiv.classList.add('question');
+
+        const questionText = document.createElement('p');
+        questionText.textContent = question.question;
+        questionDiv.appendChild(questionText);
+
+        questionDiv.innerHTML += question.input;
+
+        content.appendChild(questionDiv);
+    }
+}
