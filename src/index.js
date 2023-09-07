103067de3c457e5321e91bff8bd6e33ab5509042
OlgaLuk2911 <olya.lukash291991@gmail.com>
2023-09-07 19:21:08 +0300

    creatLastPaje

--- src/index.js
+++ src/index.js
@@ -65,3 +65,46 @@ function renderCards() {
     });
 
 }
+
+//отрисовка последней страницы
+const result = 20//условное значение результата
+function createLastPage(){
+    const content = document.getElementById('content');
+    content.innerHTML = ''
+    content.innerHTML = '<h2 class = "pinkText">Отличная работа, поздравляю!</h2>'+
+    '<div class="grid"><h3>Твой результат:</h3><h3 class = "pinkText">'+result+'%</h3></div>'
+    const divLevel = document.createElement('div')
+    divLevel.classList.add("grid")
+    content.append(divLevel)
+    divLevel.insertAdjacentHTML('beforeend',`<h3>что соответсвтвует уровню: </h3>`)
+
+    if (result<60){
+        divLevel.insertAdjacentHTML('beforeend',`<h3 class = "pinkText">"Новичок"</h3>`)
+        divLevel.insertAdjacentHTML('afterEnd',`<h2>Следует повторить:</h2>`)
+    }
+    else if (result>=60 && result<80){
+        divLevel.insertAdjacentHTML('beforeend',`<h3 class = "pinkText">"Стажёр"</h3>`)
+        divLevel.insertAdjacentHTML('afterEnd',`<h2>Ты можешь смело искать предложения по стажировке, но повтори перед этим следующие темы:</h2>`)
+    }
+    else{
+        divLevel.insertAdjacentHTML('beforeend',`<h3 class = "pinkText">"Младший разработчик"</h3>`)        
+        divLevel.insertAdjacentHTML('afterEnd',`<h2>Можешь приступать к подготовке к собеседованию!</h2><div class="div_video"><button class="btn_video">Видео с собеседованием</button></div>`)   
+    } 
+    const btnStart = document.createElement('button') 
+    btnStart.textContent = "В начало"
+    content.append(btnStart)
+
+//переход на первую страницу
+    btnStart.addEventListener('click',(e)=>{
+        content.innerHTML = ''
+        createFirstPage()
+        btnStart.style.display='none'
+    })
+} 
+
+//переход на страницу с собеседованием
+content.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('btn_video')){        
+        location.href ="https://www.youtube.com/watch?v=t3h745eJOKU"
+    }
+})
