a593a73da8faed686f0c2fae8b6bec041758d86e
OlgaLuk2911 <olya.lukash291991@gmail.com>
2023-09-16 13:43:42 +0300

    поправила код по последней странице, в css удалила лишние стили по последней странице и чуть поправила стиль по iframe

--- src/index.js
+++ src/index.js
@@ -191,41 +191,43 @@ function createLastPage(){
 
     //перевод результата в проценты
     const percentResult = result * 100/100
+    //определение уровня
+    function level() {
+        if (percentResult<=59){
+            return "\"Новичок\""
+        } else if (percentResult>=60 && percentResult<80){
+            return "\"Стажёр\""
+        } else {
+            return "\"Младший разработчик\""
+        }        
+    }
+    // рекомендации по результату 
+    function conclusion() {
+        if (percentResult<=59){
+            return `<h2>Ты можешь смело искать предложения по стажировке, но повтори перед этим следующие темы, находящиеся по ссылкам ниже:</h2>`
+        } else if (percentResult>=60 && percentResult<80){
+            return `<h2>Перед составлением резюме рекомендую сходить по нижеприведенным ссылкам и повторить темы:</h2>`
+        } else {
+            return `<h2 class="pinkText">Можешь приступать к подготовке к собеседованию!</h2>` + `<iframe src="https://www.youtube.com/embed/cyfaOAH-CF0"></iframe>`
+        } 
+    }
     //отрисовка последней страницы
     const content = document.getElementById('content');
-    //  ?: Следующая строка выполняет одинаковое действие, разный контент
-    // content.innerHTML = ''
-    content.innerHTML = `<h2 class = "pinkText">Отличная работа, поздравляю!</h2>
-            <div class="grid"><h3>Твой результат:</h3><h3 class = "pinkText">${percentResult}%</h3></div>`
-    const divLevel = document.createElement('div')
-    divLevel.classList.add("grid")
-    content.append(divLevel)
-    divLevel.insertAdjacentHTML('beforeend',`<h3>что соответсвтвует уровню: </h3>`)
-
-    if (percentResult<=59){                            // ?: не очень понятно зачем создавать эти элементы, если можно сразу сгенерить html как в куске выше, сложно читать такой код
-        divLevel.insertAdjacentHTML('beforeend',`<h3 class = "pinkText">"Новичок"</h3>`)
-        divLevel.insertAdjacentHTML('afterend',`<h2>Ты можешь смело искать предложения по стажировке, но повтори перед этим следующие темы, находящиеся по ссылкам ниже:</h2>`)
-    }
-    else if (percentResult>=60 && percentResult<80){
-        divLevel.insertAdjacentHTML('beforeend',`<h3 class = "pinkText">"Стажёр"</h3>`)
-        divLevel.insertAdjacentHTML('afterend',`<h2>Перед составлением резюме рекомендую сходить по нижеприведенным ссылкам и повторить темы:</h2>`)
-    }
-    else{
-        divLevel.insertAdjacentHTML('beforeend',`<h3 class = "pinkText">"Младший разработчик"</h3>`)
-        divLevel.insertAdjacentHTML('afterend',`<h2>Можешь приступать к подготовке к собеседованию!</h2>
-        <div class="video indent"><iframe src="https://www.youtube.com/embed/cyfaOAH-CF0"></iframe></div>`) // ?: поправила видео на более привычный формат
-    }
+    content.innerHTML = `<h2 class="pinkText">Отличная работа, поздравляю!</h2>` +
+            `<div class="grid"><h3>Твой результат:</h3><h3 class="pinkText">${percentResult}%</h3></div>` +
+            `<div class="grid"><h3>что соответсвтвует уровню:</h3><h3 class="pinkText">` + level() + `</h3></div>`+
+            `<div>` + conclusion() + `</div>`
+    
     //создание кнопки для перехода в начало
     const btnStart = document.createElement('button')
     btnStart.textContent = "В начало"
     content.append(btnStart)
 
-    //переход на первую страницу и очистка локал сторедж    // ?: не очень понятно зачем вообще очищать локал сторедж
+    //переход на первую страницу    
     btnStart.addEventListener('click',(e)=>{
         content.innerHTML = ''
         createFirstPage()
-        window.localStorage.clear()
-        btnStart.style.display='none'
+        btnStart.style.display="none"
     })
 
     if (percentResult >= 80) return '';
@@ -237,15 +239,12 @@ function createLastPage(){
             console.log(i);
             if (score <= 3) {
                 console.log(score);
-                console.log(data[key]['suggestion'][i])  // ? : очень сложно сделать нормальные стили этому куску, потому что предыдущий сделан через криейт
-                btnStart.insertAdjacentHTML('beforebegin',`<h3 class="pinkTekst">${data[key]['suggestion'][i]}</h3>`);
-
+                console.log(data[key]['suggestion'][i])  
+                btnStart.insertAdjacentHTML('beforebegin',`<h2 class="pinkTekst">${data[key]['suggestion'][i]}</h2>`);
             }
         })
     })
 }
-
-
 
 document.addEventListener("DOMContentLoaded", function () {   //Вызов функции первой страницы
     createFirstPage()
